c5978200a937390af7e47922acd1ac9c7fb3b90d
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2024-05-25 12:43:25 +0200

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use ::dsi_bitstream::prelude::*;
-use pyo3::prelude::*;
 use pyo3::exceptions::PyValueError;
+use pyo3::prelude::*;
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
 
@@ -10,7 +10,7 @@ macro_rules! impl_all {
         pub struct $reader {
             reader: BufBitReader<$endiannes, WordAdapter<u32, BufReader<File>>>,
         }
-        
+
         #[pymethods]
         impl $reader {
             #[new]
@@ -20,57 +20,81 @@ macro_rules! impl_all {
                     reader: BufBitReader::new(WordAdapter::new(BufReader::new(file))),
                 })
             }
-        
+
             fn read_bits(&mut self, n: usize) -> PyResult<u64> {
-                self.reader.read_bits(n).map_err(|e| PyValueError::new_err(e))
+                self.reader
+                    .read_bits(n)
+                    .map_err(|e| PyValueError::new_err(e))
             }
-        
+
             fn peek_bits(&mut self, n: usize) -> PyResult<u64> {
-                self.reader.peek_bits(n).map_err(|e| PyValueError::new_err(e))
+                self.reader
+                    .peek_bits(n)
+                    .map_err(|e| PyValueError::new_err(e))
             }
-        
+
             fn skip_bits(&mut self, n: usize) -> PyResult<()> {
-                self.reader.skip_bits(n).map_err(|e| PyValueError::new_err(e))
+                self.reader
+                    .skip_bits(n)
+                    .map_err(|e| PyValueError::new_err(e))
             }
-        
+
             fn read_unary(&mut self) -> PyResult<u64> {
-                self.reader.read_unary().map_err(|e| PyValueError::new_err(e))
+                self.reader
+                    .read_unary()
+                    .map_err(|e| PyValueError::new_err(e))
             }
-        
+
             fn read_gamma(&mut self) -> PyResult<u64> {
-                self.reader.read_gamma().map_err(|e| PyValueError::new_err(e))
+                self.reader
+                    .read_gamma()
+                    .map_err(|e| PyValueError::new_err(e))
             }
-            
+
             fn read_delta(&mut self) -> PyResult<u64> {
-                self.reader.read_delta().map_err(|e| PyValueError::new_err(e))
+                self.reader
+                    .read_delta()
+                    .map_err(|e| PyValueError::new_err(e))
             }
-        
+
             fn read_rice(&mut self, k: usize) -> PyResult<u64> {
-                self.reader.read_rice(k).map_err(|e| PyValueError::new_err(e))
+                self.reader
+                    .read_rice(k)
+                    .map_err(|e| PyValueError::new_err(e))
             }
-        
+
             fn read_golomb(&mut self, k: u64) -> PyResult<u64> {
-                self.reader.read_golomb(k).map_err(|e| PyValueError::new_err(e))
+                self.reader
+                    .read_golomb(k)
+                    .map_err(|e| PyValueError::new_err(e))
             }
-        
+
             fn read_zeta(&mut self, k: u64) -> PyResult<u64> {
-                self.reader.read_zeta(k).map_err(|e| PyValueError::new_err(e))
+                self.reader
+                    .read_zeta(k)
+                    .map_err(|e| PyValueError::new_err(e))
             }
-        
+
             fn read_exp_golomb(&mut self, k: usize) -> PyResult<u64> {
-                self.reader.read_exp_golomb(k).map_err(|e| PyValueError::new_err(e))
+                self.reader
+                    .read_exp_golomb(k)
+                    .map_err(|e| PyValueError::new_err(e))
             }
-        
+
             fn read_minimal_binary(&mut self, max: u64) -> PyResult<u64> {
-                self.reader.read_minimal_binary(max).map_err(|e| PyValueError::new_err(e))
+                self.reader
+                    .read_minimal_binary(max)
+                    .map_err(|e| PyValueError::new_err(e))
             }
-        
+
             fn bit_pos(&mut self) -> PyResult<u64> {
                 self.reader.bit_pos().map_err(|e| PyValueError::new_err(e))
             }
-        
+
             fn set_bit_pos(&mut self, bit_pos: u64) -> PyResult<()> {
-                self.reader.set_bit_pos(bit_pos).map_err(|e| PyValueError::new_err(e))
+                self.reader
+                    .set_bit_pos(bit_pos)
+                    .map_err(|e| PyValueError::new_err(e))
             }
         }
 
@@ -90,39 +114,57 @@ macro_rules! impl_all {
             }
 
             fn write_bits(&mut self, value: u64, n: usize) -> PyResult<usize> {
-                self.writer.write_bits(value, n).map_err(|e| PyValueError::new_err(e))
+                self.writer
+                    .write_bits(value, n)
+                    .map_err(|e| PyValueError::new_err(e))
             }
 
             fn write_unary(&mut self, value: u64) -> PyResult<usize> {
-                self.writer.write_unary(value).map_err(|e| PyValueError::new_err(e))
+                self.writer
+                    .write_unary(value)
+                    .map_err(|e| PyValueError::new_err(e))
             }
 
             fn write_gamma(&mut self, value: u64) -> PyResult<usize> {
-                self.writer.write_gamma(value).map_err(|e| PyValueError::new_err(e))
+                self.writer
+                    .write_gamma(value)
+                    .map_err(|e| PyValueError::new_err(e))
             }
 
             fn write_delta(&mut self, value: u64) -> PyResult<usize> {
-                self.writer.write_delta(value).map_err(|e| PyValueError::new_err(e))
+                self.writer
+                    .write_delta(value)
+                    .map_err(|e| PyValueError::new_err(e))
             }
 
             fn write_rice(&mut self, value: u64, k: usize) -> PyResult<usize> {
-                self.writer.write_rice(value, k).map_err(|e| PyValueError::new_err(e))
+                self.writer
+                    .write_rice(value, k)
+                    .map_err(|e| PyValueError::new_err(e))
             }
 
             fn write_golomb(&mut self, value: u64, k: u64) -> PyResult<usize> {
-                self.writer.write_golomb(value, k).map_err(|e| PyValueError::new_err(e))
+                self.writer
+                    .write_golomb(value, k)
+                    .map_err(|e| PyValueError::new_err(e))
             }
 
             fn write_zeta(&mut self, value: u64, k: u64) -> PyResult<usize> {
-                self.writer.write_zeta(value, k).map_err(|e| PyValueError::new_err(e))
+                self.writer
+                    .write_zeta(value, k)
+                    .map_err(|e| PyValueError::new_err(e))
             }
 
             fn write_exp_golomb(&mut self, value: u64, k: usize) -> PyResult<usize> {
-                self.writer.write_exp_golomb(value, k).map_err(|e| PyValueError::new_err(e))
+                self.writer
+                    .write_exp_golomb(value, k)
+                    .map_err(|e| PyValueError::new_err(e))
             }
 
             fn write_minimal_binary(&mut self, value: u64, max: u64) -> PyResult<usize> {
-                self.writer.write_minimal_binary(value, max).map_err(|e| PyValueError::new_err(e))
+                self.writer
+                    .write_minimal_binary(value, max)
+                    .map_err(|e| PyValueError::new_err(e))
             }
 
             fn flush(&mut self) -> PyResult<usize> {
